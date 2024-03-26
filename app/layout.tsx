7c682391996417eb44266045e3205f2cc19ce4e7
hel rabelo <helrabelo@gmail.com>
2024-03-26 12:47:06 -0300

    Update project files structure

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Goober - a TrashLab Assignment',
+  description: 'A simple web app to manage your rides',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
